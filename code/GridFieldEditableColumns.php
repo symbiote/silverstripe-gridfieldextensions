75c4aab0d5aa9a09de92721c4605be794e685ae2
Brice Burgess <nesta@iceburg.net>
2014-12-06 16:50:46 -0500

    support and cast relationship references as ReadonlyField

--- code/GridFieldEditableColumns.php
+++ code/GridFieldEditableColumns.php
@@ -30,7 +30,8 @@ class GridFieldEditableColumns extends GridFieldDataColumns implements
 
 		$fields = $this->getForm($grid, $record)->Fields();
 		$value  = $grid->getDataFieldValue($record, $col);
-		$field  = clone $fields->fieldByName($col);
+		$rel    = (strpos($col,'.') == false); // field references a relation value
+		$field  = ($rel) ? clone $fields->fieldByName($col) : new ReadonlyField($col);
 
 		if(!$field) {
 			throw new Exception("Could not find the field '$col'");
