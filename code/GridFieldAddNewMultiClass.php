5c1f9cca01dedbcefb8e56eedf51e0b33f7a1f9e
Felix Eggmann <flx@flxlabs.com>
2017-04-13 14:59:54 +0200

    Set unique name for AddNewMultiClass Selects
    fixing wrong behaviour when having multiple Gridfields containing AddNewMultiClass Selects on one form

--- code/GridFieldAddNewMultiClass.php
+++ code/GridFieldAddNewMultiClass.php
@@ -188,7 +188,8 @@ class GridFieldAddNewMultiClass implements GridField_HTMLProvider, GridField_URL
 
 		GridFieldExtensions::include_requirements();
 
-		$field = new DropdownField(sprintf('%s[ClassName]', __CLASS__), '', $classes, $this->defaultClass);
+
+		$field = new DropdownField(sprintf('%s[%s]', __CLASS__, $grid->getName()), '', $classes, $this->defaultClass);
 		if (Config::inst()->get('GridFieldAddNewMultiClass', 'showEmptyString')) {
 			$field->setEmptyString(_t('GridFieldExtensions.SELECTTYPETOCREATE', '(Select type to create)'));
 		}
