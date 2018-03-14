0e029a303370ac241f88f9f633aa2e8d79d5870c
Andrew Aitken-Fincham <andrew@silverstripe.com>
2018-03-14 15:18:18 +0000

    sanitise classname on addmulticlass handler

--- code/GridFieldAddNewMultiClassHandler.php
+++ code/GridFieldAddNewMultiClassHandler.php
@@ -2,16 +2,31 @@
 /**
  * A custom grid field request handler that allows interacting with form fields when adding records.
  */
-class GridFieldAddNewMultiClassHandler extends GridFieldDetailForm_ItemRequest {
+class GridFieldAddNewMultiClassHandler extends GridFieldDetailForm_ItemRequest
+{
+    public function Link($action = null)
+    {
+        if ($this->record->ID) {
+            return parent::Link($action);
+        }
 
-	public function Link($action = null) {
-		if($this->record->ID) {
-			return parent::Link($action);
-		} else {
-			return Controller::join_links(
-				$this->gridField->Link(), 'add-multi-class', get_class($this->record)
-			);
-		}
-	}
+        return Controller::join_links(
+                $this->gridField->Link(),
+                'add-multi-class',
+                $this->sanitiseClassName(get_class($this->record))
+            );
+    }
 
+    /**
+     * Sanitise a model class' name for inclusion in a link.
+     *
+     * @param mixed $class
+     *
+     * @return string
+     */
+    protected function sanitiseClassName($class)
+    {
+        return str_replace('\\', '-', $class);
+    }
 }
+
