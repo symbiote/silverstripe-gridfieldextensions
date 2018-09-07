1cd4d99b6cee161e53a41fe9ed07d48fcb8d07a0
zemiacsik <zemi@centrum.sk>
2018-09-07 10:17:01 +0200

    indent with 4 spaces instead of tab

--- _config.php
+++ _config.php
@@ -3,5 +3,5 @@
 // Ensure compatibility with PHP 7.2 ("object" is a reserved word),
 // with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
 if (!class_exists('SS_Object')) {
-  class_alias('Object', 'SS_Object');
+    class_alias('Object', 'SS_Object');
 }
