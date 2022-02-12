055279fd9ead03d9d0b3895a5c47addb7c9a75a7
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2022-02-12 23:00:36 +1300

    MNT Fix phpcs linting error.

--- src/GridFieldAddNewMultiClass.php
+++ src/GridFieldAddNewMultiClass.php
@@ -244,7 +244,12 @@ class GridFieldAddNewMultiClass implements GridField_HTMLProvider, GridField_URL
 
         GridFieldExtensions::include_requirements();
 
-        $field = DropdownField::create(sprintf('%s[%s]', __CLASS__, $grid->getName()), '', $classes, $this->defaultClass);
+        $field = DropdownField::create(
+            sprintf('%s[%s]', __CLASS__, $grid->getName()),
+            '',
+            $classes,
+            $this->defaultClass
+        );
         if (Config::inst()->get(__CLASS__, 'showEmptyString')) {
             $field->setEmptyString(_t('GridFieldExtensions.SELECTTYPETOCREATE', '(Select type to create)'));
         }
