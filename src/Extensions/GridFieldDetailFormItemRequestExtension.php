baa74139e0a225ae0837f6db82cb8c36b0a4397f
Florian Thoma <florian.thoma@innoweb.com.au>
2026-02-10 15:07:14 +1100

    update fix according to recommendation by @GuySartorelli

--- src/Extensions/GridFieldDetailFormItemRequestExtension.php
+++ src/Extensions/GridFieldDetailFormItemRequestExtension.php
@@ -28,9 +28,10 @@ class GridFieldDetailFormItemRequestExtension extends Extension
         $addMultiClassComponent = $gridFieldConfig->getComponentByType(GridFieldAddNewMultiClass::class);
         if ($addMultiClassComponent) {
             $newRecordField = static::get_new_record_field_from_actions($actions);
-            if ($newRecordField && ($container = $newRecordField->getContainerFieldList())) {
-                $container->removeByName('new-record');
-                $container->push(
+            if ($newRecordField) {
+                $containerField = $newRecordField->getContainerFieldList();
+                $containerField->removeByName('new-record');
+                $containerField->push(
                     LiteralField::create('new-record', $this->getHTMLFragment($addMultiClassComponent))
                 );
                 GridFieldExtensions::include_requirements();
